25630e915f809b4475018f13d741db81dc7a6ece
dawid <dawtan96@gmail.com>
2024-12-03 19:26:58 +0100

    Header update

---
+++ inc/minishell.h
@@ -0,0 +1,26 @@
+// Readline and history associated
+#include <readline/readline.h>
+#include <readline/history.h>
+// Printf, malloc, free, write
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+// File's operations
+#include <fcntl.h>
+#include <sys/stat.h>
+// Processes and signals
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+// Directories operations
+#include <dirent.h>
+// Error management
+#include <string.h>
+#include <errno.h>
+// TTY, Terminals
+#include <termios.h>
+#include <sys/ioctl.h>
+// Terminal controlling
+#include <termios.h>
+#include <curses.h>
+#include <term.h>
